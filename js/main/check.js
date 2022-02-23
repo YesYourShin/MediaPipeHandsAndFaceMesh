f98ba3e3773e7feeeaaab8612b669b245c161752
YesYourShin <ehrl827@naver.com>
2022-02-23 15:56:56 +0900

    merge module

---
+++ js/main/check.js
@@ -0,0 +1,93 @@
+function check(element) {
+    if (element) {
+
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const canvasCtx = canvasElement.getContext('2d');
+videoElement.style.display='none'
+function onResults(results) {
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    // 기준점을 지정한 크기(x,y)만큼 평행이동함
+    canvasCtx.translate(canvasElement.width, 0);
+    // scale(x,y)
+    // x : 수평 방향의 배율. 음수 값은 수직 축에서 픽셀을 뒤집음
+    // y : 수직 방향의 배율. 음수 값은 가로 축에서 픽셀을 뒤집음
+    canvasCtx.scale(-1, 1);
+    canvasCtx.drawImage(
+        results.image, 0, 0, canvasElement.width, canvasElement.height);
+    
+    if (results.multiHandLandmarks) {
+        right_hand = {};
+        left_hand = {};
+        text = '';
+        n = 0;
+        for (const landmarks of results.multiHandLandmarks) {
+            if (results.multiHandedness) {
+                if (results.multiHandedness[n].label == 'Right') {
+                    right_hand = landmarks;
+                }
+
+                if (results.multiHandedness[n].label == 'Left') {
+                    left_hand = landmarks;
+                }
+                n += 1;
+            }
+
+            drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
+                            {color: '#00FF00', lineWidth: 5});
+            drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2});
+            
+        }
+        canvasCtx.restore();
+        if (right_hand.length > 0 && left_hand.length > 0) {
+            if (right_hand[9]['x'] > left_hand[9]['x'] && right_hand[12]['y'] < right_hand[0]['y'] && left_hand[12]['y'] < left_hand[0]['y'] && right_hand[5]['x'] < right_hand[0]['x'] && left_hand[5]['x'] > left_hand[0]['x']) {
+                text="네";
+            }
+            else if (right_hand[9]['x'] < left_hand[9]['x'] && right_hand[12]['y'] < right_hand[0]['y'] && left_hand[12]['y'] < left_hand[0]['y'] && right_hand[9]['x'] < right_hand[0]['x'] && left_hand[9]['x'] > left_hand[0]['x']) {
+                text="아니요";
+            }
+                
+            else {
+                text="손을 정확하게 인식시켜주세요.";
+            }
+                
+        }
+        
+        else if (right_hand.length > 0 || left_hand.length > 0) {
+            text="손을 정확하게 인식시켜주세요.";
+        }
+        
+        canvasCtx.font = "40px gulim"
+        canvasCtx.fillStyle = "rgba(255,255,255,1)"
+        canvasCtx.fillText("n번에게 투표하시겠습니까?", 50, 50);
+        canvasCtx.fillText(text, 50, 100);
+
+    
+    }
+
+}
+
+const hands = new Hands({locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+}});
+hands.setOptions({
+    maxNumHands: 2,
+    modelComplexity: 1,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5
+});
+hands.onResults(onResults);
+// hands.onResults(checkThumbs);
+
+const camera = new Camera(videoElement, {
+    onFrame: async () => {
+        await hands.send({image: videoElement});
+    },
+    width: 640,
+    height: 480
+});
+camera.start();
+
+}
+}
---
+++ js/main/vote.js
@@ -0,0 +1,78 @@
+function vote(element) {
+    if (element) {
+
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const canvasCtx = canvasElement.getContext('2d');
+videoElement.style.display='none'
+function onResults(results) {
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    // 기준점을 지정한 크기(x,y)만큼 평행이동함
+    canvasCtx.translate(canvasElement.width, 0);
+    // scale(x,y)
+    // x : 수평 방향의 배율. 음수 값은 수직 축에서 픽셀을 뒤집음
+    // y : 수직 방향의 배율. 음수 값은 가로 축에서 픽셀을 뒤집음
+    canvasCtx.scale(-1, 1);
+    canvasCtx.drawImage(
+        results.image, 0, 0, canvasElement.width, canvasElement.height);
+    if (results.multiHandLandmarks) {
+            let safe = false;
+            let kill = false;
+            let text = '';
+
+        for (const landmarks of results.multiHandLandmarks) {
+            
+            if (landmarks[4]['y'] < landmarks[17]['y'] && landmarks[4]['y'] < landmarks[3]['y']){
+                kill = true;
+            }
+            
+            else if (landmarks[4]['y'] > landmarks[17]['y'] && landmarks[4]['y'] > landmarks[3]['y']){
+                safe = true;
+            }
+
+            if (kill == true){
+                text = "찬성";
+            }else if(safe == true){
+                text = "반대";
+            }
+            if (landmarks && kill == false && safe == false){
+                text = "손을 정확하게 인식시켜 주세요.";
+            }
+
+            drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
+                            {color: '#00FF00', lineWidth: 5});
+            drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2});
+        }
+        canvasCtx.restore();
+        canvasCtx.font = "40px gulim"
+        canvasCtx.fillStyle = "rgba(255,255,255,1)"
+        canvasCtx.fillText("죽이겠습니까?", 50, 50);
+        canvasCtx.fillText(text, 50, 100);
+        console.log(text);
+        
+    }
+}
+
+const hands = new Hands({locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+}});
+hands.setOptions({
+    maxNumHands: 1,
+    modelComplexity: 1,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5
+});
+hands.onResults(onResults);
+// hands.onResults(checkThumbs);
+
+// 왜 캔버스 쪽 화면 안 뜸?
+const camera = new Camera(videoElement, {
+    onFrame: async () => {
+        await hands.send({image: videoElement});
+    },
+    width: 640,
+    height: 480
+});
+camera.start();
+    }}
